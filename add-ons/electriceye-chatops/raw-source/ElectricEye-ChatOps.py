fd22de89d5caa33db44199a49b1a2fcd7cc2b6ff
bhargavak <bhargavak37@gmail.com>
2020-11-25 19:42:00 +0530

    Permissions separation

--- add-ons/electriceye-chatops/raw-source/ElectricEye-ChatOps.py
+++ add-ons/electriceye-chatops/raw-source/ElectricEye-ChatOps.py
@@ -12,26 +12,31 @@ def lambda_handler(event, context):
     # create ssm client
     ssm = boto3.client('ssm')
     # create env var for SSM Parameter containing Slack Webhook URL
-    webhookParam = os.environ['SLACK_WEBHOOK_PARAMETER']
+    notification_parameters = os.environ['NOTIFICATION_PARAMETER']
     http = urllib3.PoolManager()
     # retrieve slack webhook from SSM
     slack_hooks = []
+    teams_hooks = []
     try:
-        response = ssm.get_parameter(Name=webhookParam, WithDecryption=True)
+        response = ssm.get_parameter(Name=notification_parameters, WithDecryption=True)
         response_object = str(response['Parameter']['Value'])
         response_object_dict = json.loads(response_object)
         slack_hooks = response_object_dict.get('slack_hooks')
+        teams_hooks = response_object_dict.get('teams_hooks')
     except Exception as e:
         logger.exception(e)
-    slackHeaders = {'Content-Type': 'application/json'}
+    notification_headers = {'Content-Type': 'application/json'}
     for findings in event['detail']['findings']:
         severityLabel = str(findings['Severity']['Label'])
         electricEyeCheck = str(findings['Title'])
         awsAccountId = str(findings['AwsAccountId'])
         for resources in findings['Resources']:
             resourceId = str(resources['Id'])
-    slackMessage = 'A new ' + severityLabel + ' severity finding for ' + resourceId + ' in acccount ' + awsAccountId + ' has been created in Security Hub due to failing the check: ' + electricEyeCheck
-    message = {'text': slackMessage}
-    for webhook in slack_hooks:
-        status = http.request('POST', slackWebhook, headers=slackHeaders, body=json.dumps(message).encode('utf-8'))
-        logger.info(status)
+            Message = 'A new ' + severityLabel + ' severity finding for ' + resourceId + ' in acccount ' + awsAccountId + ' has been created in Security Hub due to failing the check: ' + electricEyeCheck
+            message = {'text': Message}
+            for webhook in slack_hooks:
+                status = http.request('POST', webhook, headers=notification_headers, body=json.dumps(message).encode('utf-8'))
+                logger.info(status)
+            for channel in teams_hooks:
+                status = http.request('POST', channel, headers=notification_headers, body=json.dumps(message).encode('utf-8'))
+                logger.info(status)
